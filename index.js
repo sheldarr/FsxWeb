070c6252d0fa01ea4734cbd2934adc760fb1a6f8
sheldar@wp.pl <sheldar@wp.pl>
2015-12-08 19:51:16 +0100

    Install superagent and react-bootstrap

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Button = require('react-bootstrap').Button;
+var Col = require('react-bootstrap').Col;
+var Glyphicon = require('react-bootstrap').Glyphicon;
+var Grid = require('react-bootstrap').Grid;
+var Nav = require('react-bootstrap').Nav;
+var Navbar = require('react-bootstrap').Navbar;
+var NavBrand = require('react-bootstrap').NavBrand;
+var NavItem = require('react-bootstrap').NavItem;
+var Row = require('react-bootstrap').Row;
+var Table = require('react-bootstrap').Table;
+
+const App = React.createClass({
+    getInitialState() {
+        return {
+            measurements: []
+        }
+    },
+
+    render() {
+        return (
+            <div>
+                <Navbar>
+                    <NavBrand>Projekt SL</NavBrand>
+                </Navbar>
+                <Grid>
+                    <Row>
+                        <Col>
+                            Test
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App></App>,
   document.getElementById('root')
 );
